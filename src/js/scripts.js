a01aff952f6e4282874e9691bdabcfebe76f214f
manisha sinha <bits.manisha@gmail.com>
2022-10-31 02:17:33 +0100

    Created scene and initaite perspectivecamera class.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -10,3 +10,13 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const orbit = new OrbitControls(camera, renderer.domElement);
+
+const axesHelper = new THREE.AxesHelper(3);
+scene.add(axesHelper);
+
+camera.position.set(-90, 140, 140);
+orbit.update();

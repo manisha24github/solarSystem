f4abe907dffdb4986ad5acaaa7fe025f0003edb2
manisha sinha <bits.manisha@gmail.com>
2022-11-01 20:05:23 +0100

    Called the map property.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,23 +1,22 @@
 import * as THREE from 'three';
-import { OrbitControls, orbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 import starsTexture from '../img/stars.jpg';
 import sunTexture from '../img/sun.jpg';
-import mercury from '../img/mercury.jpg';
-import venus from '../img/venus.jpg';
-import earth from '../img/earth.jpg';
-import mars from '../img/mars.jpg';
+import mercuryTexture from '../img//mercury.jpg';
+import venusTexture from '../img/venus.jpg';
+import earthTexture from '../img/earth.jpg';
+import marsTexture from '../img/mars.jpg';
 import jupiterTexture from '../img/jupiter.jpg';
-import saturn from '../img/saturn.jpg';
-import saturnRingTexture from '../img/saturn-ring.jpg';
+import satrunTexture from '../img/saturn.jpg';
+import satrunRingTexture from '../img/saturn-ring.jpg';
 import uranus from '../img/uranus.jpg';
 import uranusRingTexture from '../img/uranus-ring.jpg';
-import neptune from '../img/neptune.jpg';
-import pluto from '../img/pluto.jpg';
+import neptuneTexture from '../img/neptune.jpg';
+import plutoTexture from '../img/pluto.jpg';
+import { AmbientLight } from 'three';
 
 const renderer = new THREE.WebGLRenderer();
-
-renderer.shadowMap.enabled = true;
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 
@@ -28,15 +27,14 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const orbit = new OrbitControls(camera, renderer.domElement);
-
 camera.position.set(-90, 140, 140);
 orbit.update();
 
 const ambientLight = new THREE.AmbientLight(0x333333);
 scene.add(ambientLight);
 
-const cubeTextureLoader = new THREE.CubeTextureLoader();
-scene.background = cubeTextureLoader.load([
+const CubeTextureLoader = new THREE.CubeTextureLoader();
+scene.background = CubeTextureLoader.load([
   starsTexture,
   starsTexture,
   starsTexture,
@@ -46,7 +44,13 @@ scene.background = cubeTextureLoader.load([
 ]);
 
 const textureLoader = new THREE.TextureLoader();
-const sunGeo = new THREE.SphereGeometry(i6, 30, 30);
+const sunGeo = new THREE.SphereGeometry(16, 30, 30);
+const sunMat = new THREE.MeshBasicMaterial({
+  map: textureLoader.load(sunTexture),
+});
+
+const sun = new THREE.Mesh(sunGeo, sunMat);
+scene.add(sun);
 
 function animate() {
   renderer.render(scene, camera);
@@ -55,7 +59,7 @@ function animate() {
 renderer.setAnimationLoop(animate);
 
 window.addEventListener('resize', function () {
-  camera.aspect = this.window.innerWidth / this.innerHeight;
+  camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
